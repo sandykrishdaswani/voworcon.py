65653083809f7e254227ea3cd5b02e428c3c5778
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-06-26 19:12:14 +0530

    Update voworcon.py

--- voworcon.py
+++ voworcon.py
@@ -1,8 +1,8 @@
 ch=input("Enter Your Character:")
 if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'  
      or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
-  print("The Given Character", ch, "vowel")
+  print("The Given Character", ch, "Vowel")
 elif ch == '$' :
   print("The Given Character", ch, "invalid")
 else:
-  print("The Given Character", ch, "consonant")
+  print("The Given Character", ch, "Consonant")
