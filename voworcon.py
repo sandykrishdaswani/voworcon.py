2c97fee55539455d3bdf278b33a6acf957c36091
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-06-27 13:43:34 +0530

    Update voworcon.py

--- voworcon.py
+++ voworcon.py
@@ -1,7 +1,7 @@
 f=input()
 h=['a','e','i','o','u','A','E','I','O','U']
 G=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V',',W','X','Y','Z']
-if i in h:
+if f in h:
      print("Vowel")
 else:
      print("Consonant")
